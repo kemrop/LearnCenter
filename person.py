9a3adf5a08f3b34a0b8dd65390ff1aeb9757851e
Mark Rop <kemrop@gmail.com>
2013-09-26 19:27:35 -0500

    More mock unit tests

--- person.py
+++ person.py
@@ -2,28 +2,43 @@ from  car import car
 
 
 class person:
-    def __init__(self,fname=None, lname=None, age=None, gender=None, address=None, city=None, zipcode=None):
-        self.fname=fname
-        self.lname=lname
-        self.age=age
-        self.gender=gender
-        self.address=address
-        self.city=city
-        self.zipcode=zipcode
+    def __init__(self, fname=None, lname=None, age=None, gender=None, address=None, city=None, zipcode=None):
+        self.fname = fname
+        self.lname = lname
+        self.age = age
+        self.gender = gender
+        self.address = address
+        self.city = city
+        self.zipcode = zipcode
         self.name = self.fname + ' ' + self.lname
         self.car = None
-        self.car = car("Mercedez","Benze","2012", 15000, self.name)
+        self.car = car("", "", "", None , None)
 
     def get_status_quo(self):
-        car_model = self.car.get_car_make()
-        if car_model == "Mercedez":
+        car_make = self.car.get_car_make()
+        if car_make == "Mercedez":
             return 'Cool'
         else:
             return 'Not Cool'
 
 
+    def speculate_persons_age(self):
+        """
+        :rtype : object
+        """
+        car_mileage = self.car.get_car_mileage()
+        car_year = int(self.car.get_car_year())
 
+        if car_mileage <= 15001 and car_year >= 200:
+            return True
+        else:
+            return False
 
-
-
-
+    def get_full_name(self):
+        """
+        :return:
+        """
+        if self.car.get_car_model() == "Camry" and self.car.get_car_mileage() > 2000:
+            return self.fname + " " + self.lname
+        else:
+            return ""
--- unit/person_test.py
+++ unit/person_test.py
@@ -6,23 +6,54 @@ from person import person
 
 class TestCar(unittest.TestCase):
     def setUp(self):
-        self.fname="Car"
-        self.lname="Owner"
-        self.age=30
-        self.gender="Male"
-        self.address='16600 LaughAlot St #306'
-        self.city='Sioux Falls'
-        self.zipcode='57104'
+        self.fname = "Car"
+        self.lname = "Owner"
+        self.age = 30
+        self.gender = "Male"
+        self.address = '16600 LaughAlot St #306'
+        self.city = 'Sioux Falls'
+        self.zipcode = '57104'
         unittest.TestCase.setUp(self)
         self.person=person(self.fname, self.lname, self.age, self.gender, self.address, self.city, self.zipcode)
 
 
     @patch('car.car.get_car_make')
     def test_shout_cool_for_car_model_of_person(self, mock_car):
-        mock_car.return_value='Mercedez'
+        mock_car.return_value = 'Mercedez'
         actual = self.person.get_status_quo()
         expected = "Cool"
         self.assertEqual(expected, actual)
+
+    @patch('car.car.get_car_make')
+    def test_shout_cool_for_car_model_of_person(self, mock_car):
+        mock_car.return_value = 'Mazda'
+        actual = self.person.get_status_quo()
+        expected = "Not Cool"
+        self.assertEqual(expected, actual)
+
+    @patch('car.car.get_car_mileage')
+    @patch('car.car.get_car_year')
+    def test_college_graduate(self, mock_car_mileage, mock_car_year):
+        mock_car_mileage.return_value = 15001
+        mock_car_year.return_value = 1
+        actual = self.person.speculate_persons_age()
+        self.assertTrue(actual)
+
+    #Test when get Person full name only if he owns a Camry and the camry is over 15000 Mileage
+    @patch('car.car.get_car_mileage')
+    @patch('car.car.get_car_model')
+    def test_get_full_name(self, mock_car_model, mock_car_mileage):
+        mock_car_model.return_value = "Camry"
+        mock_car_mileage.return_value = 15000
+
+        expected = "Car Owner"
+        actual = self.person.get_full_name()
+
+        self.assertEqual(expected, actual)
+
+
+
+
 
 
 
